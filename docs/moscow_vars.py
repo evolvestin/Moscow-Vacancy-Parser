9bd8dce5374223c5e14503950010ed3e2d7769b7
steve10live <38049797+steve10live@users.noreply.github.com>
2020-05-20 19:09:33 +0300

    Update moscow_vars.py

--- docs/moscow_vars.py
+++ docs/moscow_vars.py
@@ -1,7 +1,6 @@
-idMe = 396978030
-tkn = '429683355:AAF3GReDyewByK-WRLQ44xpCNKIsYg1G8X0'  # 587775435:AAH-Q0Zx2joTTDZi7xkTbCJU6o2g2dtDkSA
-idMain = idMe  # -1001261829993
-idDocs = idMe # -1001470553483
-idInstagram = idMe #-1001298975823
-start_link = 9  # 10
-floater = 0  #1
+tkn = '587775435:AAH-Q0Zx2joTTDZi7xkTbCJU6o2g2dtDkSA'
+idMain = -1001261829993
+idDocs = -1001470553483
+idInstagram = -1001298975823
+start_link = 10
+floater = 1
